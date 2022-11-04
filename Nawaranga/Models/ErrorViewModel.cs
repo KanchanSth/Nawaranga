1f0e5e662034418e1cb120029eaa45ab2be85958
Kanchu <Kanchu@DESKTOP-HU1U8CQ>
2022-11-04 19:24:10 +0545

    Add project files.

---
+++ Nawaranga/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nawaranga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
